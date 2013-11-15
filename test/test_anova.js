c981786943ca497eba8d41173e5f568594db217f
Chris Jones <christopherjonesqed@gmail.com>
2013-11-15 11:29:08 -0500

    Broke anova out to a separate file.

--- test/test_anova.js
+++ test/test_anova.js
@@ -1,7 +1,7 @@
 var stat = require('../lib/main');
 
 exports['anova incorect vals opts'] = function(test) {
-  stat.anova({type: 'balanced', vals: 4}, function(results) {
+  stat.anova({type: 'oneway', vals: 4}, function(results) {
     test.ok(results.err);
     test.done();
   });
