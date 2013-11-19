eaebcaf4080129ff94600c1397eff6e68748795f
Chris Jones <christopherjonesqed@gmail.com>
2013-11-19 10:20:04 -0500

    Started mann-U

--- test.js
+++ test.js
@@ -1,33 +1,5 @@
-var numeric = require('numeric');
-
-function go(data) {
-  var obj = convertData(data);
-  var X = obj.X;
-  var Y = obj.Y;
-  var X_T = numeric.transpose(X);
-  var multipliedXMatrix = numeric.dot(X_T,X);
-  var LeftSide = numeric.inv(multipliedXMatrix);
-  var RightSide = numeric.dot(X_T,Y);
-  var Betas = numeric.dot(LeftSide,RightSide);
-  console.log(Betas);
-}
-
-function convertData(matrix) {
-  var Ym = [];
-  matrix.forEach(function(equations) {
-    Ym.push(equations[0]);
-    equations[0] = 1;
-  });
-  var obj = {
-    X: matrix,
-    Y: Ym
-  };
-  return obj;
-}
-
-go(
-  [
-    [745,36,66],[895,37,68],[442,47,64],[440,32,53],[1598,1,101]
-  ]
-  
-  );
+var mann = require('./lib/non-parametrics/mannU');
+mann.test({
+  x1: [1,2,3],
+  x2: [2,5,6]
+  }, function() {});
---
+++ test/test_nonparam.js
@@ -0,0 +1,29 @@
+var stat = require('../lib/main');
+
+exports['mann whitney U'] = function(test) {
+  var x1 = [1,4,9,6,4,3,5,6,4];
+  var x2 = [1,5,3,2,5,4,1,5];
+  var opts = {
+    type: "mann-whitney U",
+    x1: x1,
+    x2: x2
+  };
+  stat.nonparam(opts, function(retObj) {
+    test.ok(retObj.U - 48.5 < 0.00001);
+    test.equal(retObj.N1,9);
+    test.equal(retObj.N2,8);
+    test.done();
+  });
+};
+
+exports['mann whitney U wrong input'] = function(test) {
+  var opts = {
+    type: "mann-whitney U",
+    x1: 5,
+    x2: '3'
+  };
+  stat.nonparam(opts, function(err) {
+    test.equal('ERROR: options.x1 or options.x2 not array', err);
+    test.done();
+  });
+};
