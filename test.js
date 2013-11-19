0c3bb0e58c2ab8114c2b673f4a9aa556b21e9c04
Chris Jones <christopherjonesqed@gmail.com>
2013-11-19 10:30:21 -0500

    Refactored univariate to become array prototypes

--- test.js
+++
@@ -1,5 +0,0 @@
-var mann = require('./lib/non-parametrics/mannU');
-mann.test({
-  x1: [1,2,3],
-  x2: [2,5,6]
-  }, function() {});
--- test/test_univariate.js
+++ test/test_univariate.js
@@ -1,22 +1,13 @@
-var stat = require('../lib/main');
+require('../lib/main');
 
 exports.testmean = function(test) {
-  stat.mean([1,4,5,6,9], function(result) {
-    test.equal(result, 5, "univariate mean should be 5");
-    test.done();
-  });
-};
-
-exports["test mean empty array"] = function(test) {
-  stat.mean([], function(result) {
-    test.equal(result, "DIV/0 ERROR", "Should be an error");
-    test.done();
-  });
+  var result = [1,4,5,6,9].mean();
+  test.equal(result, 5, "univariate mean should be 5");
+  test.done();
 };
 
 exports["std test"] = function(test) {
-  stat.std([3, 5, 2, 4.3, 19, 333, 5, 10, 0], function(result) {
-    test.ok(result - 109.13127 < 0.001, "result is not within the acceptable epsilon");
-    test.done();
-  });
+  var result = [3, 5, 2, 4.3, 19, 333, 5, 10, 0].std();
+  test.ok(result - 109.13127 < 0.001, "result is not within the acceptable epsilon");
+  test.done();
 };
